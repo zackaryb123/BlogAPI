e17be7e4b4ecb23139377e70d8beb4f4844a10d4
Zack Blaylock <zackblaylock@gmail.com>
2017-11-22 00:14:08 -0500

    Finished Blog API
    complete

---
+++ app.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+const debug = require('debug');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+
+const BlogsRouter = require('./routes/blog-post');
+
+app.use(logger('dev'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname), '/views/layout.html');
+});
+
+app.use('/blog-post', BlogsRouter);
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Express server listening on port ${process.env.PORT || 8080}`);
+});
---
+++ routes/blog-post.js
@@ -0,0 +1,68 @@
+﻿'use strict';
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const { BlogPosts } = require('./models'); 
+
+BlogPosts.create("1st Blog", "Inital", "Zack", Date.now());
+BlogPosts.create("1st Blog", "Second", "Dom", Date.now());
+BlogPosts.create("1st Blog", "Last", "Daron", Date.now());
+
+router.get('/', function (req, res) {
+    res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+    res.status(201).json(item);
+});
+
+router.delete('/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted shopping list item \`${req.params.id}\``);
+    res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+    const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    if (req.params.id !== req.body.id) {
+        const message = (
+            `Request path id (${req.params.id}) and request body id `
+                `(${req.body.id}) must match`);
+        console.error(message);
+        return res.status(400).send(message);
+    }
+    console.log(`Updating shopping list item \`${req.params.id}\``);
+    const updatedItem = BlogPosts.update({
+        id: req.params.id,
+        title: req.body.title,
+        content: req.body.content,
+        publishDate: req.body.publishDate
+    });
+    res.status(204).end();
+});
+
+
+module.exports = router;
+
